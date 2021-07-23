25c657e086feda26d1d25691977380850f309193
rahendraputrap <putrarahendra32@gmail.com>
2021-07-23 20:36:04 +0700

    Update index.py

--- index.py
+++ index.py
@@ -6,11 +6,13 @@ input = cv2.VideoCapture(0)
 
 mpTangan = mp.solutions.hands
 tangan = mpTangan.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     ret, img = input.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     hasil = tangan.process(imgRGB)
+    print(hasil.multi_hand_landmarks)
 
     cv2.imshow("image", img)
     cv2.waitKey(1)
