eed4e6883e3bd2fe06b20507702acff9aaf9f451
rahendraputrap <putrarahendra32@gmail.com>
2021-07-23 21:05:26 +0700

    menambahkan frame rate per second

--- index.py
+++ index.py
@@ -8,6 +8,9 @@ mpTangan = mp.solutions.hands
 tangan = mpTangan.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0
+cTime = 0
+
 while True:
     ret, img = input.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -16,7 +19,13 @@ while True:
 
     if hasil.multi_hand_landmarks:
         for tanganKu in hasil.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, tanganKu)
+            mpDraw.draw_landmarks(img, tanganKu, mpTangan.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
 
     cv2.imshow("image", img)
     cv2.waitKey(1)
